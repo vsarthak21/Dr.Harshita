71ad4ea11da771790f2626a1683a8ee9c6f1961d
svashisht <sarthak.vashisht@soprasteria.com>
2020-07-26 20:19:53 +0530

    Footer changes

--- src/Components/Footerx/Footerx.js
+++ src/Components/Footerx/Footerx.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FormModal from '../FormModal/FormModal';
 import classes from "./Footerx.module.css";
 import Footer from 'rc-footer';
 import 'rc-footer/assets/index.css';
+import { common } from '@material-ui/core/colors';
+import { isThisSecond } from 'date-fns';
 
-const Footerx = () => {
-    return (
-        <div id={"contact"}>
-            <Footer
-                columns={[
-                    {
-                        icon: (
-                            <img src="https://gw.alipayobjects.com/zos/rmsportal/XuVpGqBFxXplzvLjJBZB.svg" />
-                        ),
-                        title: '语雀',
-                        url: 'https://yuque.com',
-                        description: '知识创作与分享工具',
-                        openExternal: true,
-                    },
-                ]}
-                bottom="Made with ❤️ by Sarthak"
-            />
-        </div>
-    );
+class Footerx extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showModal: false
+        }
+    }
+
+    renderModal() {
+        this.setState(() => { return { showModal: true } })
+    }
+
+    closeModal() {
+        this.setState(() => { return { showModal: false } })
+    }
+
+
+    render() {
+
+        return (
+            <div>
+                <div id={"contact"} onClick={this.renderModal.bind(this)}>
+                    <Footer
+                        columns={[
+                            {
+                                icon: (
+                                    <img src="https://gw.alipayobjects.com/zos/rmsportal/XuVpGqBFxXplzvLjJBZB.svg" />
+                                ),
+                                title: "Click here and drop your details. You will be contacted shortly!",
+                                openExternal: false,
+
+                            },
+                        ]}
+                        bottom="Made with ❤️ by Sarthak"
+                    />
+
+
+                </div>
+                <FormModal show={this.state.showModal} onHide={this.closeModal.bind(this)} />
+            </div>
+        );
+    }
 }
 export default Footerx;
